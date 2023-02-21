398b3ea3e4bbe45c25c4dabbf49abb23bb4d3190
Nahom <zerihunnahom@gmail.com>
2023-02-21 12:07:21 +0300

    Make changes to the module.exports

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
-
 const path=require("path")
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports={
-
+    entry:{
+        index:'./src/index.js'
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            title:'Output Management'
+        })
+    ],
+    output:{
+        filename:'bundle.js',
+        path:path.resolve(__dirname,'dist')
+    }
 }
